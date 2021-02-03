dd46899c58c5ab6589f59f585e68d869f3a4234c
yubachiri <k.i.moso9532@gmail.com>
2021-02-03 18:50:13 +0900

    authしてtodoのqueryまで

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -29,14 +29,14 @@ const options = {
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    //　プロバイダーは何個でも指定できる。
-    // https://next-auth.js.org/getting-started/introduction で一覧がみれる
-    // 例：
-    // Providers.Twitter({
-    //   clientId: process.env.TWITTER_CLIENT_ID,
-    //   clientSecret: process.env.TWITTER_CLIENT_SECRET,
-    // }),
   ],
   adapter: Adapters.Prisma.Adapter({ prisma }),
+  callbacks: {
+    session: async (session, user) => {
+      // user はデータベースに保存されている user オブジェクト
+      session.user.id = user.id;
+      return Promise.resolve(session);
+    },
+  },
 };
 export default (req, res) => NextAuth(req, res, options);
